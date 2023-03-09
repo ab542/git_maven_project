70a874d851f5bb761e3a12a32f9b4b1286d46f4c
ly_github <74354921+ab542@users.noreply.github.com>
2023-03-09 15:57:07 +0800

    Update GetTest.java

--- src/main/java/com/atguigu/git/GetTest.java
+++ src/main/java/com/atguigu/git/GetTest.java
@@ -6,5 +6,6 @@ public class GetTest {
         System.out.println("hello git2");
         System.out.println("hot-fex test");
         System.out.println("push test");
+         System.out.println("pull github test");
     }
 }
